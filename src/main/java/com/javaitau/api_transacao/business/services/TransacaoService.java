52f4af5855fc78a83828fd45cf9d465415524af3
GabrielH-Full <gabrielhb2020@gmail.com>
2025-08-21 01:03:05 -0300

    first commit

---
+++ src/main/java/com/javaitau/api_transacao/business/services/TransacaoService.java
@@ -0,0 +1,48 @@
+package com.javaitau.api_transacao.business.services;
+
+import com.javaitau.api_transacao.controller.dtos.TransacaoRequestDTO;
+import com.javaitau.api_transacao.infrastructure.exception.UnprocessableEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service //1
+@RequiredArgsConstructor //1 - injeçoes de indepedencias
+@Slf4j
+public class TransacaoService {
+    //armazenamento em memoria por array list
+
+    private final List<TransacaoRequestDTO> listTransacao = new ArrayList<>();
+
+    public void adicionarTransacao(TransacaoRequestDTO dto) {
+        log.info("Iniciada o processamento de gravar transações");
+
+        if(dto.dataHora().isAfter(OffsetDateTime.now())){
+            log.error("Data e Hora maiores que o atual");
+            throw new UnprocessableEntity("data e hora maiores que data e hora atuais");
+        }
+        if(dto.valor() < 0){
+            log.error("Valor negativo");
+            throw new UnprocessableEntity("Valor nao pode ser negativo");
+        }
+        listTransacao.add(dto);
+    }
+
+    public void limoparTransacao() {
+        listTransacao.clear();
+    }
+
+    public List<TransacaoRequestDTO> buscarTransacoes(Integer IntervaloBusca) {
+        log.info("Estatistica dos ultimos 60 segundos");
+        OffsetDateTime dataHoraIntervalo= OffsetDateTime.now().minusSeconds(IntervaloBusca);
+
+        return listTransacao.stream().filter(transacoes -> transacoes.dataHora()
+                .isAfter(dataHoraIntervalo)).toList();
+    }
+
+
+}
---
+++ src/test/java/com/javaitau/api_transacao/ApiTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaitau.api_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
