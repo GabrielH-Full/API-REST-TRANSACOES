276195bae5421494fd80939258066d1008885543
GabrielH-Full <gabrielhb2020@gmail.com>
2025-08-24 15:36:53 -0300

    TransacaoController

---
+++ src/main/java/com/javaitau/api_transacao/controller/TransacaoController.java
@@ -0,0 +1,36 @@
+package com.javaitau.api_transacao.controller;
+
+
+import com.javaitau.api_transacao.business.services.TransacaoService;
+import com.javaitau.api_transacao.controller.dtos.TransacaoRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/transacao")
+@Service
+public class TransacaoController {
+
+    @Autowired
+    private final TransacaoService transacaoService;
+
+    @PostMapping("/registrar")
+    public ResponseEntity<Void> receberTransacao(@RequestBody TransacaoRequestDTO dto){
+        transacaoService.adicionarTransacao(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+}
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deletarTransacao(){
+        transacaoService.limoparTransacao();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
+
+}
