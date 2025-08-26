5895fefc3acea42f8f845041d1e34f4e9a2fc005
GabrielH-Full <gabrielhb2020@gmail.com>
2025-08-26 00:49:08 -0300

    att

--- src/main/java/com/javaitau/api_transacao/controller/TransacaoController.java
+++ src/main/java/com/javaitau/api_transacao/controller/TransacaoController.java
@@ -3,6 +3,9 @@ package com.javaitau.api_transacao.controller;
 
 import com.javaitau.api_transacao.business.services.TransacaoService;
 import com.javaitau.api_transacao.controller.dtos.TransacaoRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,19 +20,29 @@ import org.springframework.web.bind.annotation.*;
 public class TransacaoController {
 
     @Autowired
-    private final TransacaoService transacaoService;
+    private TransacaoService transacaoService;
 
-    @PostMapping("/registrar")
+    @PostMapping
+    @Operation(description = "Endpoint responsavel pelas transições")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Gravação com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Criterio de trasação recusado"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro do servidor")
+    })
     public ResponseEntity<Void> receberTransacao(@RequestBody TransacaoRequestDTO dto){
         transacaoService.adicionarTransacao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
 }
-    @DeleteMapping("/delete")
+    @DeleteMapping
+    @Operation(description = "Endpoint responsavel por deletar transições")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Gravação com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro do servidor")
+    })
     public ResponseEntity<Void> deletarTransacao(){
         transacaoService.limoparTransacao();
         return ResponseEntity.status(HttpStatus.OK).build();
     }
-
-
-
 }
